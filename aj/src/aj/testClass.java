a032ef278d25b3222cc3dd3b773418436f5b24cc
ajeeth <appari.ajeeth>
2022-04-05 16:43:28 +0530

    first commit

---
+++ aj/src/aj/testClass.java
@@ -0,0 +1,16 @@
+package aj;
+
+public class testClass {
+
+	
+	public testClass() {
+	System.out.println("ajeeth");
+	}
+
+	public static void main(String[] args) {
+		new testClass();
+
+		System.out.println("is ready to use");
+	}
+
+}
